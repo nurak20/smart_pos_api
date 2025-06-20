7a53cc006fbe400987bddedd448d600d14aa0234
nurakoeurn9 <nurakoeurn9@gmail.com>
2025-06-20 18:18:10 +0700

    Improve

--- src/database/data_source.ts
+++ src/database/data_source.ts
@@ -24,8 +24,8 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    synchronize: true,
-    schema: 'rs_shop',
+    // synchronize: true,
+    // schema: 'rs_shop',
     entities: [
         Product,
         Category,
