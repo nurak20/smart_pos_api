7ae927257f2cb1a2573e1302602ef6426cd26444
nurakoeurn9 <nurakoeurn9@gmail.com>
2025-06-24 11:53:07 +0700

    Nice

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: [
-      'http://localhost:3000',        // Dev frontend
-      'https://admin.txteams.net' // Production frontend
-    ],
+    origin: true,  // This allows all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,           // if you need to send cookies/auth
     allowedHeaders: 'Content-Type, Accept, Authorization',
